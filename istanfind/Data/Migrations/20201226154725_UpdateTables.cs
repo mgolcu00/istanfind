880e2c9946ee17a145b2a0a3be42ae7c527bbee2
mgolcu <mgolcu00@gmail.com>
2020-12-26 18:49:43 +0300

    Add Models and Database Migration

---
+++ istanfind/Data/Migrations/20201226154725_UpdateTables.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace istanfind.Data.Migrations
+{
+    public partial class UpdateTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    PlaceId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FunPlace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WebSiteUrl = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    AdressUrl = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    DataText = table.Column<string>(nullable: true),
+                    TitleText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FunPlace", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HistoricalPlace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WebSiteUrl = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    AdressUrl = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    DataText = table.Column<string>(nullable: true),
+                    TitleText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoricalPlace", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hotel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WebSiteUrl = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    AdressUrl = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    DataText = table.Column<string>(nullable: true),
+                    TitleText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Park",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WebSiteUrl = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    AdressUrl = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    DataText = table.Column<string>(nullable: true),
+                    TitleText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Park", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WebSiteUrl = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    AdressUrl = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    DataText = table.Column<string>(nullable: true),
+                    TitleText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shop",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    WebSiteUrl = table.Column<string>(nullable: true),
+                    Adress = table.Column<string>(nullable: true),
+                    AdressUrl = table.Column<string>(nullable: true),
+                    Score = table.Column<double>(nullable: false),
+                    DataText = table.Column<string>(nullable: true),
+                    TitleText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shop", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "FunPlace");
+
+            migrationBuilder.DropTable(
+                name: "HistoricalPlace");
+
+            migrationBuilder.DropTable(
+                name: "Hotel");
+
+            migrationBuilder.DropTable(
+                name: "Park");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+
+            migrationBuilder.DropTable(
+                name: "Shop");
+        }
+    }
+}
---
+++ istanfind/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace istanfind.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string WebSiteUrl { get; set; }
+        public string Adress { get; set; }
+        public string AdressUrl { get; set; }
+        public double Score { get; set; }
+        public string DataText { get; set; }
+        public string TitleText { get; set; }
+
+    }
+}
