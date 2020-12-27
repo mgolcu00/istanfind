68c1af00f6cb75c4b0ddc82d16ecd91ba7319d90
akinyasar <65015917+akinyasar@users.noreply.github.com>
2020-12-27 15:45:49 +0300

    Design HomePage

---
+++ istanfind/Data/Migrations/20201227123913_DesignHomePage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace istanfind.Data.Migrations
+{
+    public partial class DesignHomePage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
