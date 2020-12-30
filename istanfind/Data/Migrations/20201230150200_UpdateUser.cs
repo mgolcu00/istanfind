4deb080fb01e342b19fb30524b880d7e4bd65522
akinyasar <65015917+akinyasar@users.noreply.github.com>
2020-12-30 19:17:29 +0300

    UpdateUser

---
+++ istanfind/Data/Migrations/20201230150200_UpdateUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace istanfind.Data.Migrations
+{
+    public partial class UpdateUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ad",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Soyad",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ad",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Soyad",
+                table: "AspNetUsers");
+        }
+    }
+}
