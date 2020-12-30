9f14952e53baec3ab0adf0924cf1f6e94daff7d7
akinyasar <65015917+akinyasar@users.noreply.github.com>
2020-12-30 21:57:31 +0300

    addControllers

---
+++ istanfind/Controllers/ShopController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using istanfind.Data;
+using istanfind.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace istanfind.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public ShopController(ApplicationDbContext context, IWebHostEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: Shops
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Shop.ToListAsync());
+        }
+
+        // GET: Shops/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            var dynmicModel = new DynmicViewModelS();
+            dynmicModel.model = shop;
+            dynmicModel.comments = await _context.Comment.Where(x => x.PlaceId == shop.Id).ToListAsync();
+            return View(dynmicModel);
+        }
+
+        // GET: Shops/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddComment([Bind("Id,Text,Score,UserId,PlaceId")] Comment comment, [Bind("Id")] Shop model)
+        {
+            if (ModelState.IsValid)
+            {
+                var path = Request.Body;
+                comment.UserId = User.Identity.Name != null ? User.Identity.Name : "non user";
+                comment.PlaceId = model.Id;
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));//Details(model.Id).Result;
+            }
+
+            return RedirectToAction(nameof(Index)); /*View(comment);*/
+        }
+        // POST: Shops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,PhoneNumber,WebSiteUrl,Adress,AdressUrl,Score,DataText,TitleText,ImageUrl")] Shop shop)
+        {
+            if (ModelState.IsValid)
+            {
+                //Resim ekleme
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(webRootPath, @"shop\hotel");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+                shop.ImageUrl = @"\images\shop\" + fileName + extension;
+
+                //********
+
+                _context.Add(shop);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shop);
+        }
+
+        // GET: Shops/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            return View(shop);
+        }
+
+        // POST: Shops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,PhoneNumber,WebSiteUrl,Adress,AdressUrl,Score,DataText,TitleText,ImageUrl")] Shop shop)
+        {
+            if (id != shop.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shop);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShopExists(shop.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shop);
+        }
+
+        // GET: Shops/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shop = await _context.Shop
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            return View(shop);
+        }
+
+        // POST: Shops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shop = await _context.Shop.FindAsync(id);
+            _context.Shop.Remove(shop);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShopExists(int id)
+        {
+            return _context.Shop.Any(e => e.Id == id);
+        }
+    }
+    public class DynmicViewModelS
+    {
+        public IEnumerable<Comment> comments { get; set; }
+        public Shop model { get; set; }
+        public Comment comment { get; set; }
+    }
+}
