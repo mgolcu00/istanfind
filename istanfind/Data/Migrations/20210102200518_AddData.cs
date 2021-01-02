59da9993e230fd1f24ec39ddaf23597396d2dd75
gamzeagirtas <gamze.agirtas@ogr.sakarya.edu.tr>
2021-01-02 23:30:48 +0300

    AddData

---
+++ istanfind/Data/Migrations/20210102200518_AddData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace istanfind.Data.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Park",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "\\images\\park\\yildizparki.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Shop",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "\\images\\shop\\emar.png");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Park",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "\\images\\park\\360istanbul.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Shop",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "\\images\\shop\\360istanbul.jpg");
+        }
+    }
+}
